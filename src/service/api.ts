064be619c4bc5eace4ed08b11e16e21da125c3df
will-souza97 <rzy.x97@gmail.com>
2022-04-10 03:09:33 -0300

    test

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// Cel 'http://192.168.43.243:3333',
-// Wifi 'http://192.168.15.8:3333',
+// Cel 'http://192.168.43.243:3000',
+// Wifi 'http://192.168.15.8:3000',
 
 export const baseURL = 'http://192.168.15.8:3333';
 
